b9e519528075021e1b8a66ce784491552756b44a
nithyasree00 <chnithya0408@gmail.com>
2026-07-08 13:11:08 +0530

    csharp program

---
+++ deepskilling/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter yout 5 - letter guess");
+        String guess = Console.ReadLine();
+        guess = (guess ?? "").ToUpper();
+        Console.WriteLine("\n your guess");
+        foreach(char letter in guess)
+        {
+            Console.WriteLine(letter);
+        }
+        Console.WriteLine();
+    }
+}
+
